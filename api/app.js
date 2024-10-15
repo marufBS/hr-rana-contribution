23dc8fe374f0d3673ef59540c64dded36bc1a088
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-10-15 23:46:26 +0600

    push

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Serve the index.html file directly
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
